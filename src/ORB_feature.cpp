ef44314cd4c4e2ea9f9d4072aa69b1f594234aea
Bonoy0328 <42759143+Bonoy0328@users.noreply.github.com>
2020-03-23 09:31:27 +0800

    ORB feature

--- src/ORB_feature.cpp
+++ src/ORB_feature.cpp
@@ -1,9 +1,11 @@
 #include "ros/ros.h"    
 #include "image_transport/image_transport.h"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/feature2d.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include "sensor_msgs/image_encodings.h"
-
+#include <chrono>
 void showView(const sensor_msgs::ImageConstPtr msgImg){
     cv_bridge::CvImagePtr cvImagePtr;
     try{
@@ -14,10 +16,23 @@ void showView(const sensor_msgs::ImageConstPtr msgImg){
     }
     cv::Mat cvColorImgMat = cvImagePtr->image;
     cv::Mat cvGrayImgMat;
+    std::vector<cv::KeyPoint> keypoints;
+    cv::Mat descriptors;
+    Ptr<cv::FeatureDetector> detector = cv::ORB::create();
+    Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
+    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    detector->detect(cvColorImgMat,keypoints);
+    descriptor->compute(cvColorImgMat,descriptors);
+    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+    ROS_INFO("extract ORB cost = %f seconds",time_used.count());
+    cv::Mat outimg;
+    cv::drawKeyPoints(cvColorImgMat,keypoints,outimg,Scalar::all(-1),cv::DrawMatchesFlags::DEFAULT);
     cv::cvtColor(cvColorImgMat,cvGrayImgMat,CV_BGR2GRAY);
     cv::imshow("colorview",cvColorImgMat);
-    cv::imshow("grayview",cvGrayImgMat);
-    cv::waitKey(5);
+    cv::imshow("ORB feature",outimg);
+    // cv::imshow("grayview",cvGrayImgMat);
+    // cv::waitKey(5);
 }
 int main(int argc, char *argv[])
 {
