6900dd5708e9197275734f6569a93bb2aefa3b2f
Bonoy0328 <937423410@qq.com>
2020-04-07 16:41:03 +0800

    test

--- src/getXYZfromPoint.cpp
+++ src/getXYZfromPoint.cpp
@@ -20,10 +20,12 @@
 // #include <Eigen/Core>
 #include <iostream>
 #include "eigen3/Eigen/Core"
-// #include "pangolin/pangolin.h"
+#include <pangolin/pangolin.h>
 // #include "pangolin/pangolin.h"
 class getXYZfromPoint
 {
+using namespace std;
+using namespace Eigen;
 private:
     ros::NodeHandle nh;
     ros::Subscriber sub;
@@ -44,8 +46,8 @@ public:
         sub = nh.subscribe("/camera/depth_registered/points",5,&getXYZfromPoint::callback,this);
     }
     void callback(const sensor_msgs::PointCloud2ConstPtr& point){
+        vector<Isometry3d,Eigen::aligned_allocator<Isometry3d>> poses;
         cnt = 0;
-
         //当前帧数据
         cv_bridge::CvImagePtr cvImagePtr;
         pcl_conversions::toPCL(*point,pcl_pc2);
@@ -159,7 +161,11 @@ public:
 
         std::cout << "R=" << std::endl << R << std::endl;
         std::cout << "t=" << std::endl << t << std::endl;
-
+        Matrix3d rotation_matrix = R;
+        Isometry3d Twr(Quaterniond(rotation_matrix));
+        Twr.pretranslate(Vector3d(t[0],t[1],t[2]));
+        poses.push_back(Twr);
+        DrawTrajectory(Twr);
         //gaussNewton 解位姿
         // getXYZfromPoint::VecVector3d pts_3d_eigen;
         // getXYZfromPoint::VecVector2d pts_2d_eigen;
@@ -250,7 +256,56 @@ Sophus::SE3d bundleAdjustmentGaussNewton(const getXYZfromPoint::VecVector3d &poi
         // std::cout << "pose by g-n \n" << pose.matrix() <<std::endl;
         return pose;
     }
-// void DrawTrajectory(std::vector<Eigen::Isometry3d>)
+void DrawTrajectory(vector<Isometry3d,Eigen::aligned_allocator<Isometry3d>> poses){
+    pangolin::CreateWindowAndBind("Trajectory Viewer", 1024, 768);
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  pangolin::OpenGlRenderState s_cam(
+    pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+    pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+  );
+
+  pangolin::View &d_cam = pangolin::CreateDisplay()
+    .SetBounds(0.0, 1.0, 0.0, 1.0, -1024.0f / 768.0f)
+    .SetHandler(new pangolin::Handler3D(s_cam));
+  while (pangolin::ShouldQuit() == false) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    d_cam.Activate(s_cam);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glLineWidth(2);
+    for (size_t i = 0; i < poses.size(); i++) {
+      // 画每个位姿的三个坐标轴
+      Vector3d Ow = poses[i].translation();
+      Vector3d Xw = poses[i] * (0.1 * Vector3d(1, 0, 0));
+      Vector3d Yw = poses[i] * (0.1 * Vector3d(0, 1, 0));
+      Vector3d Zw = poses[i] * (0.1 * Vector3d(0, 0, 1));
+      glBegin(GL_LINES);
+      glColor3f(1.0, 0.0, 0.0);
+      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glVertex3d(Xw[0], Xw[1], Xw[2]);
+      glColor3f(0.0, 1.0, 0.0);
+      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glVertex3d(Yw[0], Yw[1], Yw[2]);
+      glColor3f(0.0, 0.0, 1.0);
+      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glVertex3d(Zw[0], Zw[1], Zw[2]);
+      glEnd();
+    }
+    // 画出连线
+    for (size_t i = 0; i < poses.size(); i++) {
+      glColor3f(0.0, 0.0, 0.0);
+      glBegin(GL_LINES);
+      auto p1 = poses[i], p2 = poses[i + 1];
+      glVertex3d(p1.translation()[0], p1.translation()[1], p1.translation()[2]);
+      glVertex3d(p2.translation()[0], p2.translation()[1], p2.translation()[2]);
+      glEnd();
+    }
+    pangolin::FinishFrame();
+    usleep(5000);   // sleep 5 ms
+  }
+}
 };
 int main(int argc, char *argv[])
 {
